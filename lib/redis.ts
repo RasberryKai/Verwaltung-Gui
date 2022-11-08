bdf909ea5303b00ff6577a296657940e31f594ea
kai.kueppers <kai.kueppers@gi.com>
2022-11-08 11:17:03 +0100

    added todo

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,87 +1,86 @@
-import {Client, Entity, Schema} from "redis-om";
+import { Client, Entity, Schema } from "redis-om";
 
 const client = new Client();
 
 async function connect() {
-    if (!client.isOpen()) {
-        await client.open(process.env.REDIS_URL);
-    }
+  if (!client.isOpen()) {
+    await client.open(process.env.REDIS_URL);
+  }
 }
 
 class Movie extends Entity {}
 let movieSchema = new Schema(
-    Movie,
-    {
-        name: {type: "string"},
-        description: {type: "string"},
-        publishDate: {type: "string"},
-        category: {type: "string"},
-        condition: {type: "number"},
-        duration: {type: "number"},
-        ageRating: {type: "number"},
-        blockbuster: {type: "boolean"},
-        regisseur: {type: "string"},
-    },
-    {
-        dataStructure: "JSON",
-    }
-)
+  Movie,
+  {
+    name: { type: "string" },
+    description: { type: "string" },
+    publishDate: { type: "string" },
+    category: { type: "string" },
+    condition: { type: "number" },
+    duration: { type: "number" },
+    ageRating: { type: "number" },
+    blockbuster: { type: "boolean" },
+    regisseur: { type: "string" },
+  },
+  {
+    dataStructure: "JSON",
+  }
+);
 
 class Book extends Entity {}
 let bookSchema = new Schema(
-    Book,
-    {
-        name: {type: "string"},
-        description: {type: "string"},
-        publishDate: {type: "number"},
-        category: {type: "string"},
-        condition: {type: "number"},
-        publisher: {type: "string"},
-        author: {type: "string"},
-        isbn: {type: "string"},
-        pages: {type: "number"},
-    },
-    {
-        dataStructure: "JSON",
-    }
-)
+  Book,
+  {
+    name: { type: "string" },
+    description: { type: "string" },
+    publishDate: { type: "number" },
+    category: { type: "string" },
+    condition: { type: "number" },
+    publisher: { type: "string" },
+    author: { type: "string" },
+    isbn: { type: "string" },
+    pages: { type: "number" },
+  },
+  {
+    dataStructure: "JSON",
+  }
+);
 
 class Game extends Entity {}
-let gameSchema = new Schema(
-    Game,
-    {
-        name: {type: "string"},
-        description: {type: "string"},
-        publishDate: {type: "string"},
-        category: {type: "string"},
-        condition: {type: "number"},
-        publisher: {type: "string"},
-        developer: {type: "string"},
-        platform: {type: "string"},
-        ageRating: {type: "number"},
-    },
-)
+let gameSchema = new Schema(Game, {
+  name: { type: "string" },
+  description: { type: "string" },
+  publishDate: { type: "string" },
+  category: { type: "string" },
+  condition: { type: "number" },
+  publisher: { type: "string" },
+  developer: { type: "string" },
+  platform: { type: "string" },
+  ageRating: { type: "number" },
+});
 
 export async function createMovie(data: any) {
-    await connect();
-    const repository = client.fetchRepository(movieSchema);
+  await connect();
+  const repository = client.fetchRepository(movieSchema);
 
-    const movie = repository.createEntity(data);
-    return await repository.save(movie);
+  const movie = repository.createEntity(data);
+  return await repository.save(movie);
 }
 
 export async function createBook(data: any) {
-    await connect();
-    const repository = client.fetchRepository(bookSchema);
+  await connect();
+  const repository = client.fetchRepository(bookSchema);
 
-    const book = repository.createEntity(data);
-    return await repository.save(book);
+  const book = repository.createEntity(data);
+  return await repository.save(book);
 }
 
 export async function createGame(data: any) {
-    await connect();
-    const repository = client.fetchRepository(gameSchema);
+  await connect();
+  const repository = client.fetchRepository(gameSchema);
 
-    const game = repository.createEntity(data);
-    return await repository.save(game);
+  const game = repository.createEntity(data);
+  return await repository.save(game);
 }
+
+// TODO create methods for receiving all items
