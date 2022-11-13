ab17ba5369b7b97c672d55b1c825da7ac68f3a49
kai.kueppers <kai.kueppers@gi.com>
2022-11-13 16:54:01 +0100

    added error image

--- components/items/error.tsx
+++ components/items/error.tsx
@@ -1,3 +1,13 @@
+import Image from "next/image";
+
 export default function LoadError() {
-  return <div>Error</div>;
+  return (
+    <div className={"h-full w-full flex justify-center items-center"}>
+      <Image
+        src={require("../../public/cross.png")}
+        alt={"Error"}
+        className={"scale-50"}
+      />
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import ItemDisplay from "../components/items/itemDisplay";
 import ItemCanvas from "../components/items/ItemCanvas";
 
 export default function Home() {
-  const [filters, setFilters] = useState<string[]>([]);
-  // TODO add item list
+  const [filters, setFilters] = useState<string | null>(null);
+
   return (
     <div className={"h-screen"}>
       <InteractionRow filter={filters} setFilter={setFilters} />
