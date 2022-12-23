1ab6cd2843c31bb01d717f54fa77c888b37a99bc
kai.kueppers <kai.kueppers@gi.com>
2022-12-23 12:53:37 +0100

    removed search bar

--- components/home/InteractionRow.tsx
+++ components/home/InteractionRow.tsx
@@ -11,20 +11,10 @@ export interface TagDrillingProps {
 export default function InteractionRow(props: TagDrillingProps) {
     return (
         <div className={"w-full h-16 flex flex-row items-center"}>
-            <div className={"w-1/2"}>
+            <div className={"w-2/3"}>
                 <TagSelection filter={props.filter} setFilter={props.setFilter} />
             </div>
-            <div className={"w-1/2 flex flex-row justify-end"}>
-                <div className={"w-4/5"}>
-                    <TextInput
-                        placeholder={"Search"}
-                        rightSection={<HiMagnifyingGlass color={colors.ORANGE} size={40} className={"mr-2"} />}
-                        radius="md"
-                        className={"w-full"}
-                        size={"lg"}
-                    />
-                </div>
-            </div>
+            <div className={"w-1/2 flex flex-row justify-end"} />
         </div>
     );
 }
